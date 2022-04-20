717ffa69d704d467821a77ef92eecdf1797b21d8
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-04-20 15:03:39 -0400

    Completed exercise

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DataTypesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the rectangle: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the width of the rectangle: ");
+            double width = double.Parse(Console.ReadLine());
+            //Console.WriteLine(length.GetType());
+
+            double area = width * length;
+
+            Console.WriteLine("The area of the rectangle is: " + area);
+        }
+    }
+}
